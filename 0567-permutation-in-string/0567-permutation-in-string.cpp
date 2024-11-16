97f716da87846cf50ce4e8c45577b4d0ab9c82b7
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2024-11-16 16:52:34 +0530

    Time: 10 ms (47.04%), Space: 12.4 MB (20.37%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    // void printMp(map<char, int> mp){
-    //     for(auto& p : mp){
-    //         cout<<p.first<<":"<<p.second<<endl;
-    //     }
-    //     cout<<"khatam"<<endl;
-    // }
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size())
+        if (s1.size() == 0 || s1.size() > s2.size())
             return false;
-
-        map<char, int> mp;
-        map<char, int> temp;
-        for(char ch = 'a';ch<='z';ch++){
-            mp[ch]=0;
-            temp[ch]=0;
+        map<char, int> sOne, sTwo;
+        for (auto& ch : s1) {
+            sOne[ch]++;
         }
-        for (char ch : s1) {
-            mp[ch]++;
-        }
-
         int l = 0;
         int r = 0;
-        // printMp(mp);
-        
         while (r < s1.size()) {
-            temp[s2[r]]++;
+            sTwo[s2[r]]++;
             r++;
         }
-        if (mp == temp)
+
+        if (sOne == sTwo)
             return true;
 
         while (r < s2.size()) {
-            temp[s2[l]]--;
+            sTwo[s2[l]]--;
+            if (sTwo[s2[l]] == 0)
+                sTwo.erase(s2[l]);
             l++;
-            temp[s2[r]]++;
-            r++;
-            // printMp(temp);
-            if (mp == temp)
+
+            sTwo[s2[r]]++;
+            if (sOne == sTwo)
                 return true;
+            r++;
         }
         return false;
     }
