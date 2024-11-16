477ea4031ef0371f5e046de06f3ec59d785aa4df
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2024-11-16 16:29:40 +0530

    Time: 41 ms (13.97%), Space: 11.1 MB (39.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t.empty())
-            return "";
-        pair<int, int> ansInd;
-        int minLen = INT_MAX;
-        map<char, int> mT;
-        for (char ch : t) {
-            mT[ch]++;
+        if (t.empty()) return "";
+
+        unordered_map<char, int> countT, window;
+        for (char c : t) {
+            countT[c]++;
         }
+
+        int have = 0, need = countT.size();
+        pair<int, int> res = {-1, -1};
+        int resLen = INT_MAX;
         int l = 0;
-        map<char, int> window;
-        int have = 0, need = mT.size();
-        for (int r = 0; r < s.size(); r++) {
-            if (mT.count(s[r])) {
-                window[s[r]]++;
-                if (window[s[r]] == mT[s[r]]) {
-                    have++;
-                }
+
+        for (int r = 0; r < s.length(); r++) {
+            char c = s[r];
+            window[c]++;
+
+            if (countT.count(c) && window[c] == countT[c]) {
+                have++;
             }
+
             while (have == need) {
-                if (r - l + 1 < minLen) {
-                    minLen = r - l + 1;
-                    ansInd = {l, r};
+                if ((r - l + 1) < resLen) {
+                    resLen = r - l + 1;
+                    res = {l, r};
                 }
-                if (mT.count(s[l])) {
-                    window[s[l]]--;
-                    if (window[s[l]] < mT[s[l]]) {
-                        have--;
-                    }
+
+                window[s[l]]--;
+                if (countT.count(s[l]) && window[s[l]] < countT[s[l]]) {
+                    have--;
                 }
                 l++;
             }
         }
-        if (minLen == INT_MAX)
-            return "";
-        return s.substr(ansInd.first, minLen);
+
+        return resLen == INT_MAX ? "" : s.substr(res.first, resLen);
     }
 };
