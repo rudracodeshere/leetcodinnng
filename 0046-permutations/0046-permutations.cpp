4783ef2562ec3ee66f1649a2be6d8635531c1777
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2024-12-16 16:29:39 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,37 +1,19 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int> nums) {
-        // Base case: If the size of nums is 0, return a vector containing an empty permutation
-        if (nums.empty()) {
+    vector<vector<int>> permute(vector<int> &nums) {
+        if (nums.size() == 0)
             return {{}};
-        }
 
-        // Base case: If the size of nums is 1, return a single permutation containing nums
-        if (nums.size() == 1) {
-            return {nums};
-        }
-
-        // Take the first element out of nums to permute the rest
-        int first = nums[0];
-        vector<int> rest(nums.begin() + 1, nums.end());
-        vector<vector<int>> subAns = permute(rest);
+        vector<int> temp = vector<int>(nums.begin() + 1, nums.end());
+        vector<vector<int>> subAns = permute(temp);
         vector<vector<int>> ans;
-
-        // Insert the first element into every position of every permutation of the rest
         for (auto& sa : subAns) {
             for (int i = 0; i <= sa.size(); i++) {
-                // Insert first at position i
-                sa.insert(sa.begin() + i, first);
+                sa.insert(sa.begin() + i, nums[0]);
                 ans.push_back(sa);
-                
-                // Erase the element we just inserted to revert `sa` to its original
                 sa.erase(sa.begin() + i);
             }
         }
-
         return ans;
     }
 };
