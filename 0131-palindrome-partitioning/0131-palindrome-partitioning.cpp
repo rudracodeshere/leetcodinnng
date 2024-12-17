471a44d53fd6adec70fe70ee482e337da5d2e3cb
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2024-12-17 22:03:45 +0530

    Time: 123 ms (20.78%), Space: 140.6 MB (17.41%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,29 @@
-
-
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> sub;
-        dfs(s, 0, res, sub);
+        dfs(s, res, sub);
         return res;
     }
-    
-    void dfs(string& s, int start, vector<vector<string>>& res, vector<string>& sub) {
-        if (start == s.size()) {
+    void dfs(string s, vector<vector<string>>& res, vector<string> sub) {
+        if (s.size() == 0) {
             res.push_back(sub);
             return;
         }
-        
-        for (int end = start; end < s.size(); end++) {
-            if (isPali(s, start, end)) {
-                sub.push_back(s.substr(start, end - start + 1));
-                dfs(s, end + 1, res, sub);
+        for (int i = 0; i < s.size(); i++) {
+            string temp = s.substr(0, i + 1);
+            if (isPali(temp)) {
+                sub.push_back(temp);
+                dfs(s.substr(i + 1), res, sub);
                 sub.pop_back();
             }
         }
     }
-    
-    bool isPali(string& s, int l, int r) {
-        while (l < r) {
+    bool isPali(string& s) {
+        int l = 0;
+        int r = s.size() - 1;
+        while (l <= r) {
             if (s[l] != s[r])
                 return false;
             l++;
