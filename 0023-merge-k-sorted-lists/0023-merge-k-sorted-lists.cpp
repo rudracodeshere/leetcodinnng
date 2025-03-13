aed0eda083c89d073db0e1ef05825242d95bdd8e
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2025-03-13 19:20:19 +0530

    Time: 7 ms (33.94%), Space: 18.8 MB (33.47%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty())
-            return NULL;
-        while (lists.size() > 1) {
-            lists.push_back(merge(lists[0], lists[1]));
-            lists.erase(lists.begin());
-            lists.erase(lists.begin());
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>,
+                       greater<pair<int, ListNode*>>>
+            pq;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i])
+                pq.push({lists[i]->val, lists[i]});
         }
-        return lists[0];
-    }
-    ListNode* merge(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode();
         ListNode* temp = dummy;
-        while (l1 && l2) {
-            if (l1->val <= l2->val) {
-                temp->next = l1;
-                l1 = l1->next;
-            } else {
-                temp->next = l2;
-                l2 = l2->next;
-            }
+        while (!pq.empty()) {
+            auto p = pq.top();
+            pq.pop();
+            temp->next = p.second;
             temp = temp->next;
+            if (p.second->next)
+                pq.push({p.second->next->val, p.second->next});
         }
-        if (l1)
-            temp->next = l1;
-        if (l2)
-            temp->next = l2;
         return dummy->next;
     }
 };
