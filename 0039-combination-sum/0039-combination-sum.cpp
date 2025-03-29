930e6caa2606dcf9fcebb9b6d80a5be7b5832341
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2025-03-29 11:15:52 +0530

    Time: 1 ms (67.13%), Space: 14.1 MB (73.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        dfs(candidates, target, res, curr, 0, 0);
-        return res;
+void helper(vector<int>& candidates, int target, int index, vector<vector<int>>& ans , vector<int>&curr){
+    if(target<0 || index == candidates.size()){
+        return;
     }
-    void dfs(vector<int>& candidates, int target, vector<vector<int>>& res,
-             vector<int> curr, int sum, int i) {
-        if (sum == target) {
-            res.push_back(curr);
-            return;
-        }
-        if (i >= candidates.size() || sum > target) {
-            return;
-        }
-
-        dfs(candidates, target, res, curr, sum, i + 1);
-        curr.push_back(candidates[i]);
-        dfs(candidates, target, res, curr, sum + candidates[i], i);
+    if(target == 0){
+        ans.push_back(curr);
+        return;
+    }
+    // if(index == candidates.size()){
+    //     return;
+    // }
+    curr.push_back(candidates[index]);
+    helper(candidates,target-candidates[index], index, ans, curr);
+    curr.pop_back();
+    helper(candidates,target,index+1,ans,curr);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(candidates,target,0,ans,curr);
+        return ans;
     }
 };
