4d5ddb39afd176c4be348cc30012a51cac0f57d2
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2025-04-10 22:23:29 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int> &nums) {
-        if (nums.size() == 0)
-            return {{}};
-
-        vector<int> temp = vector<int>(nums.begin() + 1, nums.end());
-        vector<vector<int>> subAns = permute(temp);
+void helper(int index, vector<vector<int>>&ans,vector<int>&nums){
+    if(index == nums.size()){
+        ans.push_back(nums);
+    }
+    for(int i = index;i<nums.size();i++){
+        swap(nums[i],nums[index]);
+        helper(index+1,ans,nums);
+        swap(nums[i],nums[index]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        for (auto& sa : subAns) {
-            for (int i = 0; i <= sa.size(); i++) {
-                sa.insert(sa.begin() + i, nums[0]);
-                ans.push_back(sa);
-                sa.erase(sa.begin() + i);
-            }
-        }
+        helper(0,ans,nums);
         return ans;
     }
 };
