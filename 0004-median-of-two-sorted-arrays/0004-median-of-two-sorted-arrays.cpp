33d276cbc3c2f5688d3fe93284c600a41ac7a80b
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2025-04-19 17:17:26 +0530

    Time: 0 ms (100%), Space: 94.9 MB (93.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>& A = nums1;
-        vector<int>& B = nums2;
-        int total = A.size() + B.size();
-        int half = (total + 1) / 2;
-
-        if (B.size() < A.size()) {
-            swap(A, B);
-        }
-
-        int l = 0;
-        int r = A.size();
-        while (l <= r) {
-            int i = (l + r) / 2;
-            int j = half - i;
-
-            int Aleft = i > 0 ? A[i - 1] : INT_MIN;
-            int Aright = i < A.size() ? A[i] : INT_MAX;
-            int Bleft = j > 0 ? B[j - 1] : INT_MIN;
-            int Bright = j < B.size() ? B[j] : INT_MAX;
-
-            if (Aleft <= Bright && Bleft <= Aright) {
-                if (total % 2 != 0) {
-                    return max(Aleft, Bleft);
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int low = 0;
+        int high = n1;
+        int half = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = half - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) {
+                    return max(l1, l2);
                 }
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
-            } else if (Aleft > Bright) {
-                r = i - 1;
-            } else {
-                l = i + 1;
-            }
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            } else if (l1 > r2)
+                high = mid1 - 1;
+            else
+                low = mid1 + 1;
         }
-        return -1;
+        return 0;
     }
 };
